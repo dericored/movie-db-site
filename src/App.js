f93dd9f39847db59b7fffcb7477988977ab3dfb2
Derico <derrprivate@gmail.com>
2021-08-20 20:03:36 +0700

    Fixed routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Layout/Navbar";
 import { Provider } from "./context";
 import Sidebar from "./components/Layout/Sidebar";
